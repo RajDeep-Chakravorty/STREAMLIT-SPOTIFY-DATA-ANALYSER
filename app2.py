7172fadc1912ee8bc7c89c9fc0ac29e4aa5d520c
Rajdeep Chakravorty <98254554+RajDeep-Chakravorty@users.noreply.github.com>
2024-04-19 12:37:43 +0530

    Update app2.py

--- app2.py
+++ app2.py
@@ -47,7 +47,7 @@ def set_bg_from_url(image_url):
     except Exception as e:
         st.error("Error loading background image from URL: {}".format(e))
 # Call the function to set background image from URL
-image_url = "https://iili.io/J8STyhv.jpg"  # Replace with your image URL
+image_url = "https://images.deepai.org/machine-learning-models/cd8b9f6639994b569f48a599350a985a/hologram.jpg"  # Replace with your image URL
 set_bg_from_url(image_url)
 
 

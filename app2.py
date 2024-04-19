082a60c28adeeb69d663a2f1a3786d904d6acdf7
Rajdeep Chakravorty <98254554+RajDeep-Chakravorty@users.noreply.github.com>
2024-04-19 14:12:33 +0530

    Update app2.py

--- app2.py
+++ app2.py
@@ -32,7 +32,8 @@ def set_bg_from_url(image_url):
                 f"""
                 <style>
                 .stApp {{
-                    background: #f0f0f0 url('data:image/jpeg;base64,{encoded_image}') no-repeat center center;
+                    background: #f0f0f0;
+                    background-image: url('data:image/jpeg;base64,{encoded_image}');
                     background-size: cover;
                     font-family: 'Arial', sans-serif;
                     color: #333333;

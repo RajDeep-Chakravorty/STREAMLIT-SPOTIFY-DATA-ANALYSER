88f5d037340f108ef0edcd6f65d8af3112f61499
Rajdeep Chakravorty <98254554+RajDeep-Chakravorty@users.noreply.github.com>
2024-04-19 14:27:29 +0530

    Update app2.py

--- app2.py
+++ app2.py
@@ -35,7 +35,6 @@ def set_bg_from_url(image_url):
                     background-image: url('data:image/jpeg;base64,{encoded_image}');
                     background-size: cover;
                     font-family: 'Arial', sans-serif;
-                    color: #333333;
                 }}
                 </style>
                 """,
